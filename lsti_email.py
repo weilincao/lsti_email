2bc7a9d5a7121d670936c30cac8e1f9cb3658a6b
weilincao-intel <weilin.cao@intel.com>
2024-07-24 12:21:33 -0700

    add more info the regression result

--- lsti_email.py
+++ lsti_email.py
@@ -31,7 +31,28 @@ def is_test_done(rpt_file):
   return (no_status=='0'); 
 
 def get_lsti_summary(rpt_file):
-  cmd = "lsti "+rpt_file;
+  cmd = "lsti -l "+rpt_file;
+
+  output = subprocess.check_output(cmd, shell=True);
+  #output = subprocess.check_output(cmd, shell=True);
+  
+  
+  output = output.decode("ascii",errors="ignore")
+  
+  lines = output.splitlines();
+  summary = '';
+  found = False ;
+  for line in lines:
+    if( "Bucket Count Bucket Name" in line ):
+      found = True;
+    if(found):
+      summary= summary + line + "\n";
+      #if( len(line)<4):
+      #  found = 0;
+  return summary; 
+
+def get_lsti_rb_summary(rpt_file):
+  cmd = "lsti -rb -l "+rpt_file;
   output = subprocess.check_output(cmd, shell=True)
   
   
@@ -44,32 +65,38 @@ def get_lsti_summary(rpt_file):
   for line in lines:
     if( "Bucket Count Bucket Name" in line ):
       found = True;
+      summary_begin = line; 
     if(found):
-      summary= summary + line + "\n";
-      #if( len(line)<4):
-      #  found = 0;
-    line_num=line_num+1;  
+      if(line_num<75):
+        summary= summary + line + "\n";
+      else:
+        summary= summary + "\n too many failures to put inside email; go to the result directory for details\n";
+        break;
+      line_num=line_num+1;  
   return summary; 
- 
+
 ##############################################################
 
-email_list = "weilin.cao@intel.com saravanan.egambaram@intel.com weilincao@utexas.edu";
+email_list = "weilin.cao@intel.com saravanan.egambaram@intel.com rohit1.singh@intel.com rama.krishna.vyata@intel.com parth.a.patel@intel.com  keytaek.lee@intel.com kailesh.bavaria@intel.com jim.ming.lim@intel.com  brandon.l.smalley@intel.com adelene.kah.jia.huong@intel.com";
+#email_list = "weilin.cao@intel.com";
 results_dir = sys.argv[1];
 results_dir = subprocess.check_output("realpath "+results_dir, shell=True).decode('utf-8').strip() + "/";
+results_base = subprocess.check_output( ("basename " + results_dir), shell=True).decode('utf-8').strip();
+
 rpt =results_dir + "*.rpt" ;
 minute=0;
-while(is_test_done(rpt)==False and minute<= (60*24) ):
-  time.sleep(60);
-  print("test not yet completed");
-  minute += 1;
+while(is_test_done(rpt)==False and minute < (24*60) ):
+  time.sleep(60*10);
+  #print("tests not yet completed");
+  minute += 10;
 
-if (minute<(60*24)):
+if (minute< (24*60) ):
   print("all test completed, sending emails to subscribers");
 else:
   print("tests been running over 24 hours; not all test completed, sending emails to subscribers");
 #print(get_lsti_summary(rpt) );
-content = "Please see the result directory at:"+results_dir +"\nBelow is the lsti summary:\n\n"+ get_lsti_summary(rpt) + "\n\nThis is an automated msg, please reach out to weilin.cao@intel.com for any question\n";
-content = content.replace("\n","\\n").replace("\t","\\t").replace('"',"'");
-cmd = 'echo -e "'+ content  +  '" | '+ 'mail -s "Regression Result" '+ email_list;
+content = "Please see the result directory at:"+results_dir +"\nBelow is the lsti summary:\n\n"+ get_lsti_summary(rpt) + "\n\nBucket Details:\n\n" + get_lsti_rb_summary(rpt)  +  "\n\n\nThis is an automated msg, please reach out to weilin.cao@intel.com for any question\n";
+content = content.replace("\n","\\n").replace("\t","\\t").replace('"',"'").replace("`","");
+cmd = 'echo -e "'+ content  +  '" | '+ 'mail -s "Regression Result for '+ results_base + '" '+ email_list;
 os.system(cmd);
 
